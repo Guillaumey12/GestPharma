886dacfe9f18904d19816802ed32fed17cab9723
Amour SODJINOU <asodjnou@afytech.com>
2021-07-02 10:46:30 +0100

    Gestion des Utilisateurs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth/signup', canActivate:[AuthGuardService], component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'user-list', canActivate:[AuthGuardService], component: UserListComponent },
+  { path: 'updateUser/:id', canActivate:[AuthGuardService], component: UpdateUserComponent },
+  { path: '', redirectTo: 'accueil', pathMatch: 'full'},
+  { path: '**', redirectTo: 'accueil' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserService } from './services/user.service';
+import { UpdateUserComponent } from './update-user/update-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    AccueilComponent,
+    UserListComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ 
+    AuthService,
+    AuthGuardService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
